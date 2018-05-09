4877de39546d748a8b61afcac9f4f15befabd125
gordas <vakononika@mail.ru>
2018-05-10 03:38:08 +0400

    fix
    Icons changed from image tu svg format

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -6,8 +6,8 @@ import styled from 'styled-components'
 
 import '../styles/main.scss'
 
-import ChatIcon from '../../assets/svg/navbar/Chat.svg'
-import NotificationIcon from '../../assets/svg/navbar/Notification.svg'
+import BellIcon from './BellIcon';
+import ChatIcon from './ChatIcon';
 
 const Navbar = styled.span`
   display: flex;
@@ -77,9 +77,6 @@ const UserMenu = styled.div`
   margin-left: 60px;
 `
 
-const Icon = styled.img`
-  margin-right: 15px;
-`
 const AvatarContainer = styled.div`
   margin-right: 16px;
   position: relative;
@@ -102,6 +99,18 @@ const Avatar = styled.img`
   width: 37px;
   height: 37px;
   border-radius: 50%;
+`
+
+const Icon = styled.div`
+  margin-right: 15px;
+  svg {
+      fill: ${props => {
+        if (props.primary) return '#007bff';
+        else if(props.green) return '#06C953';
+        else if(props.red) return '#dc3545';
+        else return '#2F3033';
+      }}
+  }
 `
 
 const NavBar = props => (
@@ -139,8 +148,18 @@ const NavBar = props => (
         </RegisterMenu>
       ) : (
         <UserMenu>
-          <Icon src={ChatIcon} alt="" />
-          <Icon src={NotificationIcon} alt="" />
+          <Icon primary>
+            <ChatIcon />
+          </Icon>
+          <Icon>
+            <ChatIcon />
+          </Icon>
+          <Icon green>
+            <BellIcon />
+          </Icon>
+          <Icon red>
+            <BellIcon />
+          </Icon>
           <AvatarContainer>
             <Avatar src={props.user.avatar} alt="" />
           </AvatarContainer>
