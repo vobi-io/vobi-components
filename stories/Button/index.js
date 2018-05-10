db6de1caea0e0de8aa1b34e6065d7e4af1bc0b63
gordas <vakononika@mail.ru>
2018-05-10 20:40:01 +0400

    fix Replaced old buttons story with new one

--- stories/Button/index.js
+++ stories/Button/index.js
@@ -9,33 +9,140 @@ import { Button } from '../../src'
 
 import themes from '../themes'
 
+const spanStyle = {
+  marginLeft: "20px"
+}
+
 storiesOf('Buttons', module)
   .addDecorator(withKnobs)
   .add(
     'Button',
     withInfo(`
-      Button component info
+      Default, Primary, Secondary, Green, Red, Yellow, Dark Buttons with Many Options
+      Don't Use Together Primary, Secondary, Green, Red, Yellow, Dark or Custom
+      Don't Use Together Solid and Round,
+      Don't Use Together Sm and Lg,
     `)(() => (
       <div>
-        <ThemeProvider
-          theme={themes[select(
-            'Theme', {
-              defaultTheme: 'Default Theme',
-              bookingGeniusTheme: 'Booking Genius Theme',
-            },
-            'defaultTheme',
-          )]}
-        >
           <Button
             onClick={action('button-click')}
-            primary={boolean('Primary', false)}
             solid={boolean('Solid', false)}
-            flat={boolean('Flat', false)}
+            rounded={boolean('Rounded', false)}
             block={boolean('Block', false)}
+            disabled={boolean('Disabled', false)}
+            bold={boolean('Bold', false)}
+            sm={boolean('Sm', false)}
+            lg={boolean('Lg', false)}
+            outline={boolean('Outline', false)}
           >
-            {text('Default', 'Button Label')}
+            {text('Default', 'Default')}
           </Button>
-        </ThemeProvider>
+          <span style={spanStyle}></span>
+          <Button
+            onClick={action('button-click')}
+            primary
+            solid={boolean('Solid', false)}
+            rounded={boolean('Rounded', false)}
+            block={boolean('Block', false)}
+            disabled={boolean('Disabled', false)}
+            bold={boolean('Bold', false)}
+            sm={boolean('Sm', false)}
+            lg={boolean('Lg', false)}
+            outline={boolean('Outline', false)}
+          >
+            {text('Default', 'Primary')}
+          </Button>
+          <span style={spanStyle}></span>
+          <Button
+            onClick={action('button-click')}
+            secondary
+            solid={boolean('Solid', false)}
+            rounded={boolean('Rounded', false)}
+            block={boolean('Block', false)}
+            disabled={boolean('Disabled', false)}
+            bold={boolean('Bold', false)}
+            sm={boolean('Sm', false)}
+            lg={boolean('Lg', false)}
+            outline={boolean('Outline', false)}
+          >
+            {text('Default', 'Secondary')}
+          </Button>
+          <span style={spanStyle}></span>
+          <Button
+            onClick={action('button-click')}
+            green
+            solid={boolean('Solid', false)}
+            rounded={boolean('Rounded', false)}
+            block={boolean('Block', false)}
+            disabled={boolean('Disabled', false)}
+            bold={boolean('Bold', false)}
+            sm={boolean('Sm', false)}
+            lg={boolean('Lg', false)}
+            outline={boolean('Outline', false)}
+          >
+            {text('Default', 'Green')}
+          </Button>
+          <span style={spanStyle}></span>
+          <Button
+            onClick={action('button-click')}
+            red
+            solid={boolean('Solid', false)}
+            rounded={boolean('Rounded', false)}
+            block={boolean('Block', false)}
+            disabled={boolean('Disabled', false)}
+            bold={boolean('Bold', false)}
+            sm={boolean('Sm', false)}
+            lg={boolean('Lg', false)}
+            outline={boolean('Outline', false)}
+          >
+            {text('Default', 'Red Button')}
+          </Button>
+          <span style={spanStyle}></span>
+          <Button
+            onClick={action('button-click')}
+            yellow
+            solid={boolean('Solid', false)}
+            rounded={boolean('Rounded', false)}
+            block={boolean('Block', false)}
+            disabled={boolean('Disabled', false)}
+            bold={boolean('Bold', false)}
+            sm={boolean('Sm', false)}
+            lg={boolean('Lg', false)}
+            outline={boolean('Outline', false)}
+          >
+            {text('Default', 'Yellow')}
+          </Button>
+          <span style={spanStyle}></span>
+          <Button
+            onClick={action('button-click')}
+            dark
+            solid={boolean('Solid', false)}
+            rounded={boolean('Rounded', false)}
+            block={boolean('Block', false)}
+            disabled={boolean('Disabled', false)}
+            bold={boolean('Bold', false)}
+            sm={boolean('Sm', false)}
+            lg={boolean('Lg', false)}
+            outline={boolean('Outline', false)}
+          >
+            {text('Default', 'Dark')}
+          </Button>
+          <span style={spanStyle}></span>
+          <Button
+            bg={text('Background-Color', 'tomato')}
+            color={text('Text-Color', '#f0f0f0')}
+            onClick={action('button-click')}
+            solid={boolean('Solid', false)}
+            rounded={boolean('Rounded', false)}
+            block={boolean('Block', false)}
+            disabled={boolean('Disabled', false)}
+            bold={boolean('Bold', false)}
+            sm={boolean('Sm', false)}
+            lg={boolean('Lg', false)}
+            outline={boolean('Outline', false)}
+          >
+            {text('Default', 'Custom')}
+          </Button>
       </div>
     )),
   )
