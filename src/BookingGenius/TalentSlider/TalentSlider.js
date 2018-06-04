56790b40df455c1964256b76c6e0f1a9d7e18c7c
vako <vakononika@mail.ru>
2018-06-04 11:51:55 +0300

    feat: added talent slider component

--- src/BookingGenius/TalentSlider/TalentSlider.js
+++ src/BookingGenius/TalentSlider/TalentSlider.js
@@ -3,26 +3,380 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 import Slider from "react-slick"
-import "~../../../assets/svg/BookingGenius/slick/slick.css"; 
-import "~../../../assets/svg/BookingGenius/slick/slick-theme.css";
 
 import { Rating } from '../..'
-import DollarIcon from '../../../assets/svg/BookingGenius/Dollar.svg'
+import Prev from '../../../assets/svg/BookingGenius/Prev.svg'
+import Next from '../../../assets/svg/BookingGenius/Next.svg'
+
+
+const Img = styled.img`
+    display: block;   
+    cursor: pointer;
+    margin-top: 79px;
+`
+
+
+const PrevArrow = ({onClick}) => (
+    <Img src={Prev} onClick={onClick} />
+)
+
+const NextArrow = ({onClick}) => (
+    <Img src={Next} onClick={onClick} />
+)
+
+const Container = styled.div`
+    .slick-slider
+    {
+        position: relative;
+
+        display: block;
+        box-sizing: border-box;
+
+        -webkit-user-select: none;
+        -moz-user-select: none;
+            -ms-user-select: none;
+                user-select: none;
+
+        -webkit-touch-callout: none;
+        -khtml-user-select: none;
+        -ms-touch-action: pan-y;
+            touch-action: pan-y;
+        -webkit-tap-highlight-color: transparent;
+    }
+
+    .slick-list
+    {
+        position: relative;
+
+        display: block;
+        overflow: hidden;
+
+        margin: 0;
+        padding: 0;
+    }
+    .slick-list:focus
+    {
+        outline: none;
+    }
+    .slick-list.dragging
+    {
+        cursor: pointer;
+        cursor: hand;
+    }
+
+    .slick-slider .slick-track,
+    .slick-slider .slick-list
+    {
+        -webkit-transform: translate3d(0, 0, 0);
+        -moz-transform: translate3d(0, 0, 0);
+            -ms-transform: translate3d(0, 0, 0);
+            -o-transform: translate3d(0, 0, 0);
+                transform: translate3d(0, 0, 0);
+    }
+
+    .slick-track
+    {
+        position: relative;
+        top: 0;
+        left: 0;
+
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    .slick-track:before,
+    .slick-track:after
+    {
+        display: table;
+
+        content: '';
+    }
+    .slick-track:after
+    {
+        clear: both;
+    }
+    .slick-loading .slick-track
+    {
+        visibility: hidden;
+    }
+
+    .slick-slide
+    {
+        display: none;
+        float: left;
+
+        height: 100%;
+        min-height: 1px;
+    }
+    [dir='rtl'] .slick-slide
+    {
+        float: right;
+    }
+    .slick-slide img
+    {
+        display: block;
+    }
+    .slick-slide.slick-loading img
+    {
+        display: none;
+    }
+    .slick-slide.dragging img
+    {
+        pointer-events: none;
+    }
+    .slick-initialized .slick-slide
+    {
+        display: block;
+    }
+    .slick-loading .slick-slide
+    {
+        visibility: hidden;
+    }
+    .slick-vertical .slick-slide
+    {
+        display: block;
+
+        height: auto;
+
+        border: 1px solid transparent;
+    }
+    .slick-arrow.slick-hidden {
+        display: none;
+    }
+
+    .slick-prev,
+    .slick-next
+    {
+        font-size: 0;
+        line-height: 0;
+
+        position: absolute;
+        top: 50%;
+
+        display: block;
+
+        width: 20px;
+        height: 20px;
+        padding: 0;
+        -webkit-transform: translate(0, -50%);
+        -ms-transform: translate(0, -50%);
+        transform: translate(0, -50%);
+
+        cursor: pointer;
+
+        color: transparent;
+        border: none;
+        outline: none;
+        background: transparent;
+    }
+    .slick-prev:hover,
+    .slick-prev:focus,
+    .slick-next:hover,
+    .slick-next:focus
+    {
+        color: transparent;
+        outline: none;
+        background: transparent;
+    }
+    .slick-prev:hover:before,
+    .slick-prev:focus:before,
+    .slick-next:hover:before,
+    .slick-next:focus:before
+    {
+        opacity: 1;
+    }
+    .slick-prev.slick-disabled:before,
+    .slick-next.slick-disabled:before
+    {
+        opacity: .25;
+    }
+
+    .slick-prev:before,
+    .slick-next:before
+    {
+        font-family: 'slick';
+        font-size: 20px;
+        line-height: 1;
+
+        opacity: .75;
+        color: white;
+
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+
+    .slick-prev
+    {
+        left: -25px;
+    }
+    [dir='rtl'] .slick-prev
+    {
+        right: -25px;
+        left: auto;
+    }
+    .slick-prev:before
+    {
+        content: '←';
+    }
+    [dir='rtl'] .slick-prev:before
+    {
+        content: '→';
+    }
+
+    .slick-next
+    {
+        right: -25px;
+    }
+    [dir='rtl'] .slick-next
+    {
+        right: auto;
+        left: -25px;
+    }
+    .slick-next:before
+    {
+        content: '→';
+    }
+    [dir='rtl'] .slick-next:before
+    {
+        content: '←';
+    }
+
+    /* Dots */
+    .slick-dotted.slick-slider
+    {
+        margin-bottom: 30px;
+    }
+
+    .slick-dots
+    {
+        position: absolute;
+        bottom: -25px;
+
+        display: block;
+
+        width: 100%;
+        padding: 0;
+        margin: 0;
+
+        list-style: none;
+
+        text-align: center;
+    }
+    .slick-dots li
+    {
+        position: relative;
+
+        display: inline-block;
+
+        width: 20px;
+        height: 20px;
+        margin: 0 5px;
+        padding: 0;
+
+        cursor: pointer;
+    }
+    .slick-dots li button
+    {
+        font-size: 0;
+        line-height: 0;
+
+        display: block;
+
+        width: 20px;
+        height: 20px;
+        padding: 5px;
+
+        cursor: pointer;
+
+        color: transparent;
+        border: 0;
+        outline: none;
+        background: transparent;
+    }
+    .slick-dots li button:hover,
+    .slick-dots li button:focus
+    {
+        outline: none;
+    }
+    .slick-dots li button:hover:before,
+    .slick-dots li button:focus:before
+    {
+        opacity: 1;
+    }
+    .slick-dots li button:before
+    {
+        font-family: 'slick';
+        font-size: 6px;
+        line-height: 20px;
+
+        position: absolute;
+        top: 0;
+        left: 0;
+
+        width: 20px;
+        height: 20px;
+
+        content: '•';
+        text-align: center;
+
+        opacity: .25;
+        color: black;
+
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+    .slick-dots li.slick-active button:before
+    {
+        opacity: .75;
+        color: black;
+    }
+
+`
+
+const Override = styled.div`
+    .slick-slider {
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-start;
+        max-width: 1200px;
+        margin: 0 auto;
+    }
+
+    .slick-list {
+        width: 1080px;
+    }
+
+    .slick-slide {
+        display: flex !important;
+    }
+
+    .slick-current ~ .slick-slide {
+        justify-content: center; 
+    }
+
+    .slick-current ~ .slick-slide ~.slick-slide {
+        justify-content: flex-end; 
+    }
+
+    .slick-current {
+        justify-content: flex-start !important;
+    }
+`
 
 const Heading = styled.h2`
     font-family: Raleway;
     font-size: 22px;
     font-weight: bold;
-    line-height: 66px;
     color: #2F3033;
-    margin: 0 0 2px;
+    margin: 0 auto 22px;
     padding: 0;
+    width: 1080px;
+    text-transform: capitalize;
 `
 
 const Column = styled.div`
     display: flex;
     flex-direction: column;
-
 `
 const Main = Column.extend`
     width: 350px !important;
@@ -62,6 +416,10 @@ const Prof = styled.span`
     text-transform: uppercase;
 `
 
+const SelfEnd = styled.div`
+    align-self: flex-end;
+`
+
 const Review = styled.span`
     font-family: Lato;
     display: inline-block;
@@ -76,32 +434,34 @@ class TalentSlider extends React.Component {
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3,
-        //autoplay: true,
-        //autoplaySpeed: 3500,
-        //nextArrow: <NextArrow />,
-        //prevArrow: <PrevArrow />
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
       };
       return (
-        <div>
-          <Heading>{this.props.heading}</Heading>
-          <Slider {...settings}>
-            {this.props.artistList.map((artist, index) => (
-                <Main key={index}>
-                    <Image src={artist.img} />
-                    <Desc>
-                        <Column>
-                            <Prof>{artist.profession}</Prof>
-                            <Name>{artist.fullName}</Name>
-                        </Column>
-                        <Column>
-                            <Rating value={artist.rating} disabled={false} size={18} style={{ alignSelf: 'flex-end'}}/>
-                            <Review>{`${artist.reviewCount} reviews`}</Review>
-                        </Column>
-                    </Desc>
-                </Main>
-            ))}
-          </Slider>
-        </div>
+        <Container>
+            <Override>
+                <Heading>{this.props.heading}</Heading>
+                <Slider {...settings}>
+                    {this.props.artistList.map((artist, index) => (
+                        <Main key={index}>
+                            <Image src={artist.img} />
+                            <Desc>
+                                <Column>
+                                    <Prof>{artist.profession}</Prof>
+                                    <Name>{artist.fullName}</Name>
+                                </Column>
+                                <Column>
+                                    <SelfEnd>
+                                        <Rating value={artist.rating} disabled={false} size={18} />
+                                    </SelfEnd>
+                                    <Review>{`${artist.reviewCount} reviews`}</Review>
+                                </Column>
+                            </Desc>
+                        </Main>
+                    ))}
+                </Slider>
+            </Override>
+        </Container>
       );
     }
   }
@@ -109,54 +469,6 @@ class TalentSlider extends React.Component {
 TalentSlider.propTypes = {
     artistList: PropTypes.array.isRequired,
     heading: PropTypes.string.isRequired
-}
-
-TalentSlider.defaultProps = {
-    artistList: [
-        {
-            img: 'https://i.imgur.com/yl34Ayi.png',
-            profession: 'dancer',
-            fullName: 'David Lakovsky',
-            rating: 5,
-            reviewCount: 93
-        },
-        {
-            img: 'https://i.imgur.com/19HpIUV.png',
-            profession: 'singer',
-            fullName: 'Hue Montorello',
-            rating: 1,
-            reviewCount: 98
-        },
-        {
-            img: 'https://i.imgur.com/pb0Zb3S.png',
-            profession: 'dj',
-            fullName: 'Lui Nesh',
-            rating: 4,
-            reviewCount: 134
-        },
-        {
-            img: 'https://i.imgur.com/5IXinO1.png',
-            profession: 'singer',
-            fullName: 'Elisabeth Vekkel',
-            rating: 3,
-            reviewCount: 255
-        },
-        {
-            img: 'https://i.imgur.com/Sx75sVi.png',
-            profession: 'magician',
-            fullName: 'Henry Celigber',
-            rating: 5,
-            reviewCount: 23
-        },
-        {
-            img: 'https://i.imgur.com/eF9yQFF.png',
-            profession: 'dj',
-            fullName: 'Pola Makkensy',
-            rating: 2,
-            reviewCount: 55
-        }
-    ],
-    heading: 'All Talents'
 }
 
 export default TalentSlider
--- stories/BookingGenius/index.js
+++ stories/BookingGenius/index.js
@@ -25,6 +25,51 @@ import VideoPreviewModalExample from './VideoPreviewModalExample'
 import SignupModalExample from './SignupModalExample'
 import SocialSignupModalExample from './SocialSignupModalExample'
 
+const artistData = [
+  {
+      img: 'https://i.imgur.com/yl34Ayi.png',
+      profession: 'dancer',
+      fullName: 'David Lakovsky',
+      rating: 5,
+      reviewCount: 93
+  },
+  {
+      img: 'https://i.imgur.com/19HpIUV.png',
+      profession: 'singer',
+      fullName: 'Hue Montorello',
+      rating: 1,
+      reviewCount: 98
+  },
+  {
+      img: 'https://i.imgur.com/pb0Zb3S.png',
+      profession: 'dj',
+      fullName: 'Lui Nesh',
+      rating: 4,
+      reviewCount: 134
+  },
+  {
+      img: 'https://i.imgur.com/5IXinO1.png',
+      profession: 'singer',
+      fullName: 'Elisabeth Vekkel',
+      rating: 3,
+      reviewCount: 255
+  },
+  {
+      img: 'https://i.imgur.com/Sx75sVi.png',
+      profession: 'magician',
+      fullName: 'Henry Celigber',
+      rating: 5,
+      reviewCount: 23
+  },
+  {
+      img: 'https://i.imgur.com/eF9yQFF.png',
+      profession: 'dj',
+      fullName: 'Pola Makkensy',
+      rating: 2,
+      reviewCount: 55
+  }
+]
+
 const newsArray = [
   {
     id: 0,
@@ -435,5 +480,8 @@ storiesOf('BookingGenius', module)
     </div>
   ))
   .add('Talent Slider', () => (
-    <TalentSlider />
+      <TalentSlider 
+        heading={'hot talents'}
+        artistList={artistData}
+      />
   ))
