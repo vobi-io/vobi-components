8cd0ef408feef635b13224da027522cc37d0749f
vako <vakononika@mail.ru>
2018-11-12 13:16:36 +0300

    fix: modal

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -3,11 +3,35 @@ import PropTypes from 'prop-types'
 import ReactModal from 'react-modal'
 import styled from 'styled-components'
 
+const styles = {
+  overlay: {
+    backgroundColor: 'rgba(216, 216, 216, 0.7)',
+    border: 0,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  content: {
+    backgroundColor: '#ffffff',
+    boxShadow: '0 1px 12px 0 rgba(0, 0, 0, 0.21)',
+    border: 0,
+    borderRadius: 0,
+    width: 'max-content',
+    position: 'relative',
+    maxHeight: '80vh',
+    overflowY: 'auto',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+}
+
 const Modal = props => (
   <ReactModal
     isOpen={props.isOpen}
     closeTimeoutMS={0}
-    style={{ overlay: props.styleOverlay, content: props.styleContent }}
+    style={{ overlay: styles.overlay, content: styles.content }}
     contentLabel={props.contentLabel}
     ariaHideApp={false}
     shouldFocusAfterRender
@@ -29,15 +53,11 @@ Modal.propTypes = {
   isOpen: PropTypes.bool,
   contentLabel: PropTypes.string,
   children: PropTypes.node.isRequired,
-  styleOverlay: PropTypes.object,
-  styleContent: PropTypes.object,
 }
 
 Modal.defaultProps = {
   isOpen: false,
   contentLabel: '',
-  styleOverlay: {},
-  styleContent: {},
 }
 
 export default Modal
